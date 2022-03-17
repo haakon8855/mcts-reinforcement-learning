a0d6c961bf855bec68b305e8e97ff032d300a857
Håkon <haakon8855@users.noreply.github.com>
2022-03-17 12:03:49 +0100

    Added NIM simworld class (empty)

---
+++ game_nim.py
@@ -0,0 +1,22 @@
+"""Haakon8855"""
+
+
+class GameNim:
+    """
+    Implementation of the NIM game as a simworld.
+    """
+
+    def __init__(self):
+        self.test = "hei"
+        print(self.test)
+
+
+def main():
+    """
+    Main function for running this python script.
+    """
+    simworld = GameNim()
+
+
+if __name__ == "__main__":
+    main()
